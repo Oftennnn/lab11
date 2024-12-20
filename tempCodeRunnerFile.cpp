96efe0e148d43196419e8a8af58e37a312748a64
Oftennnn <inwture133@gmail.com>
2024-12-20 16:56:42 +0700

    lab11 finish

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -68,7 +68,7 @@ int main(){
 	do{
 		cout << "(1) Destiny draw (2) Stay, SELECT: ";
 		cin >> playerAction;
-	}while(playerAction != 1 && playerAction != 2); //This line of code is not completed. You need to set the condition to do loop if user's input is not 1 or 2. 
+	}while(playerAction != (1 || 2)); //This line of code is not completed. You need to set the condition to do loop if user's input is not 1 or 2. 
 	if(playerAction == 1){
 		//The following lines of code are not completed. Please correct it.
 		
